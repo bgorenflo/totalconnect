36bf1177f48eb624e5beaec5010e2ad779dffc4d
bgorenflo <bgorenflo@gmail.com>
2015-04-23 12:40:49 -0400

    total connect website addy updated

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,255 @@
+// Called as soon as application is ready. It initializes data.
+Pebble.addEventListener("ready", function(e) {
+    console.log("Initializing thermostat data ...");
+    initializeThermostatData();
+});
+
+// Receives a message from the watch with data to change temperature
+Pebble.addEventListener("appmessage", function(e) {
+    var temperatureChange = parseInt(e.payload.temperatureChange),
+        thermostatIndex = parseInt(e.payload.thermostatIndex),
+        thermostatId = e.payload.thermostatId;
+
+    changeTemperature(thermostatId, temperatureChange, thermostatIndex);
+});
+
+// Changes the temperature of a thermostat adding temperatureChange to
+// its current temperature. Returns error in case of failure.
+function changeTemperature(thermostatId, temperatureChange, thermostatIndex){
+    console.log("Requesting temperature change: " + temperatureChange
+        + ", for thermostat: " + thermostatId);
+
+    login(function(){
+        getTemperature(thermostatId, function(currentTemperature, success){
+            var thermostatData = {"thermostatIndex": thermostatIndex},
+                newTemperature;
+
+            if (!success){
+                thermostatData.thermostatName = "Error";
+                Pebble.sendAppMessage(thermostatData);
+                return;
+            }
+
+            newTemperature = currentTemperature + temperatureChange;
+
+            setTemperature(thermostatId, newTemperature, function(success){
+                if (success){
+                    thermostatData.thermostatTemperature =
+                            encodeTemperature(newTemperature);
+                }
+                else {
+                    thermostatData.thermostatName = "Error";
+                }
+
+                Pebble.sendAppMessage(thermostatData);
+            });
+        })
+    });
+}
+
+// Gets the temperature of a thermostat
+function getTemperature(thermostatId, callbackFn){
+    ajaxCall({
+        url: 'https://mytotalconnectcomfort.com/portal/Device/Control'
+            + thermostatId,
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        callback: function(){
+            var success = true,
+                temperature = 0,
+                response = this.responseText;
+
+            try {
+                temperature = JSON.parse(response).latestData.uiData.CoolSetpoint;
+                console.log("Thermostat " + thermostatId
+                    + " current temperature: " + temperature);
+            }
+            catch (e){
+                console.error('Unable to get current temperature for thermostat '
+                    + thermostatId + ". Error: " + e);
+                console.log("Response text: " + response);
+                success = false;
+            }
+
+            callbackFn(temperature, success);
+        }
+    });
+}
+
+// Sets the temperature of a thermostat
+function setTemperature(thermostatId, temperature, callbackFn){
+    ajaxCall({
+        url: 'https://mytotalconnectcomfort.com/portal/Device/Control',
+        params: JSON.stringify({
+            DeviceID: parseInt(thermostatId),
+            CoolSetPoint: temperature
+        }),
+        method: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        callback: function(){
+            var response = this.responseText,
+                success;
+
+            try {
+                success = response && JSON.parse(response).success;
+                if (!success) {
+                    throw response;
+                }
+                console.log("Temperature updated!");
+            }
+            catch (e){
+                console.error('Unable to set temperature for thermostat '
+                    + thermostatId + ". Error: " + e);
+                success = false;
+            }
+
+            callbackFn(success);
+        }
+    });
+}
+
+// Logs in into the Honeywell site using the credentials saved in localStorage
+function login(callbackFn){
+    console.log("Connecting as user: "+localStorage.honeywellUsername);
+    ajaxCall({
+        url: 'https://mytotalconnectcomfort.com/portal/',
+        params: 'UserName=' + localStorage.honeywellUsername
+            + '&Password=' + localStorage.honeywellPassword
+            + '&timeOffset=240',
+        method: 'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        callback: callbackFn
+    });
+}
+
+// Initializes the thermostat data with login response data
+function initializeThermostatData(){
+    login(function(){
+        var parsedResponse = parseLoginResponse(this.responseText),
+            thermostats = parsedResponse.thermostats,
+            thermostatsCount = thermostats.length,
+            thermostat,
+            i;
+
+        if (thermostatsCount > 0){
+            for (i = 0; i < thermostatsCount; i++){
+                thermostat = {
+                    "thermostatIndex": i,
+                    "thermostatId": thermostats[i].id,
+                    "thermostatName": thermostats[i].name,
+                };
+
+                // Get the target (desired) temperature per each thermostat and
+                // send the data for each thermostat to the watch at 1 second intervals
+                setTimeout(function(thermostatData){
+                    getTemperature(thermostatData.thermostatId, function(temperature){
+                        thermostatData.thermostatTemperature =
+                            encodeTemperature(temperature);
+                        Pebble.sendAppMessage(thermostatData);
+                    });
+                }, i*1000, thermostat);
+            }
+        }
+        else {
+            console.log("Error when trying to log in Honeywell site.");
+            console.log("Response text: " + this.responseText);
+            console.log("Response status: " + this.status);
+            console.log("Response Errors: " + parsedResponse.errors.join());
+        }
+    });
+}
+
+// Parses the login call response. Returns the thermostats data if the
+// call was successful. Otherwise returns the errors.
+function parseLoginResponse(htmlString){
+    var response = document.createElement("div"),
+        thermostats = [],
+        errors = [],
+        nodeList,
+        nodeListLength,
+        i;
+
+    response.innerHTML = htmlString;
+    nodeList = response.querySelectorAll('[data-id]');
+    nodeListLength = nodeList.length;
+
+    for (i = 0; i < nodeListLength; i++){
+        thermostats.push({
+            id: nodeList[i].getAttribute('data-id'),
+            indoorTemperature: parseInt(nodeList[i].querySelector('.tempValue')
+                .innerText.trim()),
+            name: nodeList[i].querySelector('.location-name').innerText.trim()
+                .toLowerCase().replace(/^./, function(m){return m.toUpperCase();})
+        });
+    }
+
+    if (nodeListLength < 1){
+        nodeList = response.getElementsByClassName('validation-summary-errors');
+        nodeListLength = nodeList.length;
+        for (i = 0; i < nodeListLength; i++){
+            errors.push(nodeList[i].innerHTML);
+        }
+    }
+
+    return {
+        thermostats: thermostats,
+        errors: errors
+    }
+}
+
+// Converts temperature to string and adds symbol °
+function encodeTemperature(temperature){
+    return temperature + "\u00B0"; // Unicode for °
+}
+
+// -------------- Configuration ----------------
+// This configuration section generates an html 
+// form on the fly to save into HTML5 local storage
+// the username and password of the Honeywell
+// website that receives commands to query and
+// update the thermostat data.
+// Once they are saved, they are used by default
+// to submit those commands
+
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("Showing configuration");
+  Pebble.openURL('data:text/html,<html> <head> <meta name="viewport" content="width=device-width, initial-scale=1"> <style> * {font-family:verdana; font-size: 20px} input {border: 2px solid #a1a1a1; border-radius: 5px;} label, .grp {display: block; padding: 5px;} h1 {background: #85A3FF; padding: 10px} </style> </head> <body> <h1>Honeywell Site Credentials</h1> <form action="pebblejs://close#"> <div class="grp"><label>Username:</label><input type="email" name="u" placeholder="user@email.com" value="'+(localStorage.honeywellUsername||'')+'" ></div> <div class="grp"><label>Password:</label><input type="password" name="p"></div> <div class="grp" style="padding-top:30px;"> <input type="submit" onclick="var f=document.forms[0], params={\'username\': f.u.value, \'password\': f.p.value}; f.action += encodeURIComponent(JSON.stringify(params));"> <input type="submit" value="Cancel"> </div> </form> </body> </html><!--.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  var params = JSON.parse(decodeURIComponent(e.response));
+
+  // Store credentials in a localStorage object
+  if (params.username && params.password){
+    localStorage.honeywellUsername = params.username;
+    localStorage.honeywellPassword = params.password;
+    console.log("Stored credentials for: " + localStorage.honeywellUsername);
+  }
+});
+// ----------- End of Configuration -------------
+
+
+// A helper function to make ajax Calls
+function ajaxCall(options){
+    var xhr = new XMLHttpRequest(),
+        method = (options.method || 'GET').toUpperCase(),
+        headers = options.headers,
+        params = options.params,
+        url = options.url + (method === 'GET' && params ? '?' + params : '');
+
+    xhr.open(method, url);
+
+    // Set headers
+    if (headers){
+        Object.keys(headers).forEach(function(key){
+            xhr.setRequestHeader(key, headers[key]);
+        });
+    }
+
+    xhr.onload = options.callback;
+    xhr.onerror = function(e){ console.error(e); }
+
+    xhr.send(method === 'POST' ? params : null);
+}
---
+++ src/thermostat_setter.c
@@ -0,0 +1,213 @@
+#include <pebble.h>
+
+// The maximum number of items in the array of thermostats
+#define MAX_THERMOSTATS 2
+
+// Data associated with a thermostat
+struct thermostat {
+  char id[20];
+  char name[50];
+  char temperature[6];
+};
+
+// Array that holds data for all thermostats
+struct thermostat thermostats[MAX_THERMOSTATS] = {
+  {"1", "Loading ...", "0°"},
+  {"2", "Loading ...", "0°"}
+};
+
+// The currently selected thermostat
+static int selected_thermostat = 0;
+
+static Window *s_window;
+static GBitmap *s_res_up;
+static GBitmap *s_res_selector;
+static GBitmap *s_res_down;
+static GBitmap *s_res_thermometer;
+static GFont s_res_bitham_42_bold;
+static GFont s_res_gothic_28;
+static ActionBarLayer *actionbarlayer;
+static BitmapLayer *bitmaplayer;
+static TextLayer *temperaturelayer;
+static TextLayer *namelayer;
+
+// Updates the UI with current data from the array of thermostats
+static void update_ui(void){
+  text_layer_set_text(temperaturelayer, thermostats[selected_thermostat].temperature);
+  text_layer_set_text(namelayer, thermostats[selected_thermostat].name);
+}
+
+// The keys for the messages send from/to the phone. Corresponds to the
+// AppKeys in appinfo.json
+enum {
+  TEMPERATURE_CHANGE_KEY,
+  THERMOSTAT_INDEX_KEY,
+  THERMOSTAT_ID_KEY,
+  THERMOSTAT_NAME_KEY,
+  THERMOSTAT_TEMPERATURE_KEY
+};
+
+// Process the message received in the watch from the phone
+// Updates the array of thermostats with the received data
+static void receive_message(DictionaryIterator *iter, void *context) {
+  Tuple *thermostat_id_tuple;
+  Tuple *thermostat_name_tuple;
+  Tuple *thermostat_temperature_tuple;
+  Tuple *thermostat_index_tuple = dict_find(iter, THERMOSTAT_INDEX_KEY);
+  int i;
+
+  if (thermostat_index_tuple){
+    i = thermostat_index_tuple->value->uint8;
+    if (i < MAX_THERMOSTATS){ // Make sure the array does not overflow
+
+      thermostat_id_tuple = dict_find(iter, THERMOSTAT_ID_KEY);
+      if (thermostat_id_tuple){
+        strcpy(thermostats[i].id, thermostat_id_tuple->value->cstring);
+      }
+
+      thermostat_name_tuple = dict_find(iter, THERMOSTAT_NAME_KEY);
+      if (thermostat_name_tuple){
+        strcpy(thermostats[i].name, thermostat_name_tuple->value->cstring);
+      }
+
+      thermostat_temperature_tuple = dict_find(iter, THERMOSTAT_TEMPERATURE_KEY);
+      if (thermostat_temperature_tuple) {
+        strcpy(thermostats[i].temperature,
+          thermostat_temperature_tuple->value->cstring);
+      }
+
+      update_ui();
+    }
+  }
+}
+
+// Sends message from the watch to the phone
+static void send_message(int temperature_change) {
+  DictionaryIterator *iter;
+
+  app_message_outbox_begin(&iter);
+
+  if (iter == NULL) {
+    return;
+  }
+
+  Tuplet thermostat_id_tuple =
+    TupletCString(THERMOSTAT_ID_KEY, thermostats[selected_thermostat].id);
+  dict_write_tuplet(iter, &thermostat_id_tuple);
+
+  Tuplet thermostat_index_tuple =
+    TupletInteger(THERMOSTAT_INDEX_KEY, selected_thermostat);
+  dict_write_tuplet(iter, &thermostat_index_tuple);
+
+  Tuplet temperature_change_tuple =
+    TupletInteger(TEMPERATURE_CHANGE_KEY, temperature_change);
+  dict_write_tuplet(iter, &temperature_change_tuple);
+
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  selected_thermostat = (selected_thermostat + 1) % MAX_THERMOSTATS;
+  update_ui();
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(namelayer, "Raising ...");
+  send_message(1);
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(namelayer, "Lowering ...");
+  send_message(-1);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void initialize_ui(void) {
+  s_window = window_create();
+  window_set_background_color(s_window, GColorBlack);
+  window_set_fullscreen(s_window, false);
+  
+  s_res_up = gbitmap_create_with_resource(RESOURCE_ID_UP);
+  s_res_selector = gbitmap_create_with_resource(RESOURCE_ID_SELECTOR);
+  s_res_down = gbitmap_create_with_resource(RESOURCE_ID_DOWN);
+  s_res_thermometer = gbitmap_create_with_resource(RESOURCE_ID_THERMOMETER);
+  s_res_bitham_42_bold = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+  s_res_gothic_28 = fonts_get_system_font(FONT_KEY_GOTHIC_28);
+
+  // actionbarlayer
+  actionbarlayer = action_bar_layer_create();
+  action_bar_layer_add_to_window(actionbarlayer, s_window);
+  action_bar_layer_set_background_color(actionbarlayer, GColorWhite);
+  action_bar_layer_set_icon(actionbarlayer, BUTTON_ID_UP, s_res_up);
+  action_bar_layer_set_icon(actionbarlayer, BUTTON_ID_SELECT, s_res_selector);
+  action_bar_layer_set_icon(actionbarlayer, BUTTON_ID_DOWN, s_res_down);
+  action_bar_layer_set_click_config_provider(actionbarlayer, click_config_provider);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)actionbarlayer);
+  
+  // bitmaplayer
+  bitmaplayer = bitmap_layer_create(GRect(5, 0, 41, 94));
+  bitmap_layer_set_bitmap(bitmaplayer, s_res_thermometer);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)bitmaplayer);
+  
+  // temperaturelayer
+  temperaturelayer = text_layer_create(GRect(40, 16, 78, 42));
+  text_layer_set_background_color(temperaturelayer, GColorBlack);
+  text_layer_set_text_color(temperaturelayer, GColorWhite);
+  text_layer_set_text_alignment(temperaturelayer, GTextAlignmentRight);
+  text_layer_set_font(temperaturelayer, s_res_bitham_42_bold);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)temperaturelayer);
+  
+  // namelayer
+  namelayer = text_layer_create(GRect(8, 93, 100, 56));
+  text_layer_set_background_color(namelayer, GColorBlack);
+  text_layer_set_text_color(namelayer, GColorWhite);
+  text_layer_set_font(namelayer, s_res_gothic_28);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)namelayer);
+
+  update_ui();
+}
+
+static void destroy_ui(void) {
+  window_destroy(s_window);
+  action_bar_layer_destroy(actionbarlayer);
+  bitmap_layer_destroy(bitmaplayer);
+  text_layer_destroy(temperaturelayer);
+  text_layer_destroy(namelayer);
+  gbitmap_destroy(s_res_up);
+  gbitmap_destroy(s_res_selector);
+  gbitmap_destroy(s_res_down);
+  gbitmap_destroy(s_res_thermometer);
+}
+
+static void handle_window_unload(Window* window) {
+  destroy_ui();
+}
+
+static void show_main_window(void) {
+  initialize_ui();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .unload = handle_window_unload,
+  });
+  window_stack_push(s_window, true);
+}
+
+static void initialize_messaging(void) {
+  app_message_register_inbox_received(receive_message);
+
+  const int inbound_size = 64;
+  const int outbound_size = 64;
+  app_message_open(inbound_size, outbound_size);
+}
+
+int main(void) {
+  initialize_messaging();
+  show_main_window();
+  app_event_loop();
+}
